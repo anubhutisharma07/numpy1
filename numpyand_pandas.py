7258072637337e0c285bd8b37f5a886e3073d0af
Anubhuti Sharma <138015308+anubhutisharma07@users.noreply.github.com>
2023-12-19 18:15:45 +0530

    Add files via upload

---
+++ numpyand_pandas.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+simple_list=(6,7,9)
+np.array(simple_list)
+
+
+# In[3]:
+
+
+arr=np.array([1,2,3])
+arr
+
+
+# In[4]:
+
+
+list_of_lists=([1,2,3],[4,5,6],[7,8,9])
+np.array(list_of_lists)
+
+
+# In[5]:
+
+
+np.arange(5,10)
+
+
+# In[6]:
+
+
+np.arange(1,100)
+
+
+# In[7]:
+
+
+np.arange(1,31,5)
+
+
+# In[8]:
+
+
+np.arange(5)
+
+
+# In[9]:
+
+
+np.zeros(10)
+
+
+# In[10]:
+
+
+np.zeros(10,int)
+
+
+# In[11]:
+
+
+np.ones((2,3))
+
+
+# In[12]:
+
+
+np.ones(100)
+
+
+# In[13]:
+
+
+np.zeros((2,5),int)
+
+
+# In[14]:
+
+
+np.ones((2,5))
+
+
+# In[15]:
+
+
+np.ones((2,5),int)
+
+
+# In[ ]:
+
+
+
+
